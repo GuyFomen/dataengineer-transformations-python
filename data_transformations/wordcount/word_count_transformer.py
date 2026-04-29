69d941a01888d82b2a472396fb81a8e0897f4517
Guy Fomen <46876225+GuyFomen@users.noreply.github.com>
2026-04-29 16:16:27 -0400

    interview round

--- data_transformations/wordcount/word_count_transformer.py
+++ data_transformations/wordcount/word_count_transformer.py
@@ -1,11 +1,14 @@
 import logging
 
 from pyspark.sql import SparkSession
+from pyspark.sql import functions as F  # noqa: N812
 
 
 def run(spark: SparkSession, input_path: str, output_path: str) -> None:
     logging.info("Reading text file from: %s", input_path)
     input_df = spark.read.text(input_path)
+    input_df = input_df.select(F.explode(F.split(F.col("value"), " ")).alias("word"))
+    input_df = input_df.groupBy("word").count()
 
     logging.info("Writing csv to directory: %s", output_path)
 
